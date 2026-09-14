816caee7e5826013f4310c2f3ce6ffe8f8f7cd38
medex736-debug <medex736@gmail.com>
2026-09-14 19:35:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/car-detailing-studio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
